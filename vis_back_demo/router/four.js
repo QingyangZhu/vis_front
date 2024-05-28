c6bc376aa7df236a5b6822029b4532adf436c5c7
QingyangZhu <qingyang297@outlook.com>
2024-05-28 18:39:53 +0800

    净利率、净资产收益率、总资产报酬率数据上传

--- vis_back_demo/router/four.js
+++ vis_back_demo/router/four.js
@@ -1,9 +1,18 @@
 let express = require('express');
 let router = express.Router();
 
+let TARData = require('../mock/totalAssertReturnRate.json');
+let NARData = require('../mock/netAssertReturnRate.json');
+let NPMData = require('../mock/netProfitMargin.json');
+
 /* GET home page. */
 router.get('/data', (req,res)=> {
-    res.send({msg:"one.js的路由地址"})
+    res.send({
+        msg:"获取净利率、净资产收益率、总资产报酬率",
+        TARData:TARData,
+        NARData:NARData,
+        NPMData:NPMData
+    })
 })
 
 module.exports = router;
